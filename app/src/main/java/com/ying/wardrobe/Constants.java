20796f7d3029b517f3f682bfe961910b85743bb4
liucy <liucy@mapbar.com>
2020-05-12 23:49:54 +0800

    衣橱UI更新

---
+++ app/src/main/java/com/ying/wardrobe/Constants.java
@@ -0,0 +1,6 @@
+package com.ying.wardrobe;
+
+public class Constants {
+    public static final String BaseUrl="http://coding-space.cn:9032/";
+
+}
--- app/src/main/java/com/ying/wardrobe/activity/LoginActivity.java
+++ app/src/main/java/com/ying/wardrobe/activity/LoginActivity.java
@@ -1,15 +1,27 @@
 package com.ying.wardrobe.activity;
 
 import com.ying.wardrobe.BaseActivity;
+import com.ying.wardrobe.Constants;
 import com.ying.wardrobe.R;
+import com.ying.wardrobe.okhttp.OkHttpUtils;
+import com.ying.wardrobe.okhttp.callback.Callback;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import okhttp3.Call;
+import okhttp3.Response;
 
 public class LoginActivity extends BaseActivity implements View.OnClickListener {
+    private static final String TAG = "LoginActivity";
     private TextView registerTextView,loginTextView;
+    private EditText userEditText,passwordEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +36,8 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
     protected void initView() {
         registerTextView=findViewById(R.id.registerTextView);
         loginTextView=findViewById(R.id.loginTextView);
+        userEditText=findViewById(R.id.userEditText);
+        passwordEditText=findViewById(R.id.passwordEditText);
         registerTextView.setOnClickListener(this);
         loginTextView.setOnClickListener(this);
     }
@@ -37,10 +51,35 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.registerTextView:
-                Intent intent=new Intent();
-                intent.setClass(this,MainActivity.class);
-                startActivity(intent);
-                this.finish();
+                if (TextUtils.isEmpty(userEditText.getText().toString().trim())||TextUtils.isEmpty(passwordEditText.getText().toString().trim())){
+                    Toast.makeText(this,"账号密码不要忘记输入哦！",Toast.LENGTH_LONG).show();
+                }
+                OkHttpUtils.post()
+//                        .url("https://www.baidu.com")
+                        .url(Constants.BaseUrl+"api/user/register")
+                        .addParams("username",userEditText.getText().toString().trim())
+                        .addParams("password",passwordEditText.getText().toString().trim())
+                        .build().execute(new Callback() {
+                    @Override
+                    public Object parseNetworkResponse(Response response, int id) throws Exception {
+                        return null;
+                    }
+
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.d(TAG, "onError: "+e.toString());
+                    }
+
+                    @Override
+                    public void onResponse(Object response, int id) {
+                        Log.d(TAG, "onResponse: "+response.toString());
+                        Intent intent=new Intent();
+                        intent.setClass(LoginActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        LoginActivity.this.finish();
+                    }
+                });
+
                 break;
             case R.id.loginTextView:
                 Intent loginIntent=new Intent();
